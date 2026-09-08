d1df7439415aed4fe119323f75dfd637df08378d
Josh Walsh <josh.walsh@the-refinery.io>
2026-09-08 17:15:07 -0400

    Fix for add product form style

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'block w-full rounded-lg border border-gray-200 px-3 py-2 leading-6 placeholder-gray-500 focus:border-blue-500 focus:outline-none focus:ring-3 focus:ring-blue-500/50 disabled:opacity-50']) }}>
+<input @disabled($disabled) {{ $attributes->merge(['class' => 'rounded-lg border border-gray-200 px-3 py-2 leading-6 placeholder-gray-500 focus:border-blue-500 focus:outline-none focus:ring-3 focus:ring-blue-500/50 disabled:opacity-50']) }}>
